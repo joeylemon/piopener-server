6abb111f024039a1f8800db98cda26da81dabd40
joeylemon <joeyclemon@gmail.com>
2021-04-06 17:07:57 +0000

    log env

--- src/routes/admin/admin.test.js
+++ src/routes/admin/admin.test.js
@@ -3,6 +3,8 @@ import should from 'should' // eslint-disable-line no-unused-vars
 import { BASE_URL } from '../../utils/constants.js'
 import * as config from '../../utils/config.js'
 
+console.log("env other_token", process.env.OTHER_TOKEN)
+
 const api = supertest.agent(BASE_URL)
 const token = process.env.OTHER_TOKEN ? process.env.OTHER_TOKEN : config.get('other_token')
 
