ed6b31b3f2e05cf4d4baf2bd6cfb51fb5b0f28f2
joeylemon <joeyclemon@gmail.com>
2021-04-13 15:53:51 +0000

    add region exit time test

--- src/routes/garage/garage.test.js
+++ src/routes/garage/garage.test.js
@@ -17,4 +17,30 @@ describe('Garage Endpoints', () => {
             })
             .catch(err => done(err)) // eslint-disable-line promise/no-callback-in-promise
     })
+
+    it('should update the region exit time of the user', done => {
+        let previousExitTime
+        api
+            .get(`/getregionexittime/${token}`)
+            .expect(200)
+            .then(async res => {
+                previousExitTime = parseInt(res.text)
+                if (isNaN(previousExitTime)) {
+                    return done(new Error(`could not parse int: ${res.text}`)) // eslint-disable-line promise/no-callback-in-promise
+                }
+
+                await api.get(`/updateregionexittime/${token}`).expect(200)
+                return api.get(`/getregionexittime/${token}`).expect(200)
+            })
+            .then(res => {
+                const newExitTime = parseInt(res.text)
+                if (isNaN(newExitTime)) {
+                    return done(new Error(`could not parse int: ${res.text}`)) // eslint-disable-line promise/no-callback-in-promise
+                }
+
+                previousExitTime.should.not.eql(newExitTime)
+                return done() // eslint-disable-line promise/no-callback-in-promise
+            })
+            .catch(err => done(err)) // eslint-disable-line promise/no-callback-in-promise
+    })
 })
